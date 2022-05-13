db545f6ac371031668b754b664bebdcc51dbca1a
Rubin Timilsina <rubintimilsina@Rubins-MacBook-Pro.local>
2022-05-13 14:56:53 +0545

    Upgraded

--- snake game/game.py
+++ snake game/game.py
@@ -21,7 +21,7 @@ clock = pygame.time.Clock()
  
 snake_block = 10
 snake_speed = 15
- 
+ #fuck you
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
  
